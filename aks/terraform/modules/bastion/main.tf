015adbbc526fe046c951b8a5f69fa5f386cd4d76
James Ellwood <james.ellwood@solace.com>
2024-02-13 09:56:51 -0500

    Use standard sku for bastion host public ip for AKS.

--- aks/terraform/modules/bastion/main.tf
+++ aks/terraform/modules/bastion/main.tf
@@ -10,6 +10,7 @@ resource "azurerm_public_ip" "bastion" {
   location            = var.region
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_security_group" "bastion" {
