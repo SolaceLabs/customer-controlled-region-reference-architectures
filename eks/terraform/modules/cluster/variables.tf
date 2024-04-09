b2a8460ca09b1d128a6e24b6751fe26a77b8bc66
James Ellwood <james.ellwood@solace.com>
2024-04-09 08:51:49 -0400

    Support for 'access entries' for authentication in EKS clusters (#39)

--- eks/terraform/modules/cluster/variables.tf
+++ eks/terraform/modules/cluster/variables.tf
@@ -25,7 +25,7 @@ variable "kubernetes_version" {
 
 variable "kubernetes_service_cidr" {
   type        = string
-  default     = "10.100.0.0/16"
+  default     = null
   description = "The CIDR used to assign IPs to kubernetes services, internal to the cluster."
 }
 
@@ -45,4 +45,16 @@ variable "kubernetes_api_authorized_networks" {
   type        = list(string)
   default     = []
   description = "The list of CIDRs that can access the Kubernetes API, in addition to the bastion host and worker nodes (which are added by default)."
+}
+
+variable "kubernetes_cluster_auth_mode" {
+  type        = string
+  default     = null
+  description = "This controls which authentication method to use for the cluster. See the readme for more details."
+}
+
+variable "kubernetes_cluster_admin_arns" {
+  type        = list(string)
+  default     = []
+  description = "When kubernetes_cluster_auth_mode is set to 'API', user or role ARNs can be provided that will be given assigned AmazonEKSClusterAdminPolicy for this cluster."
 }
