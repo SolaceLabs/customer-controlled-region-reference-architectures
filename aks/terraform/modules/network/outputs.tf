f525d40c808c609bd0ef26e8212efc134eaf8fe4
nagsubhrajitt <Subhrajit.Nag@solace.com>
2026-04-14 20:03:18 -0600

    Make changes

--- aks/terraform/modules/network/outputs.tf
+++ aks/terraform/modules/network/outputs.tf
@@ -9,3 +9,7 @@ output "secondary_subnet_id" {
 output "route_table_id" {
   value = var.create_network ? azurerm_route_table.cluster[0].id : null
 }
+
+output "virtual_network_name" {
+  value = var.create_network ? azurerm_virtual_network.this[0].name : null
+}
